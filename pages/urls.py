c6837e522b5aa1eac775ab10abc40073844d2406
Mokowz <ronniemokaya30@gmail.com>
2023-08-25 20:40:17 +0300

    Customize Update and Detail views functionality

--- pages/urls.py
+++ pages/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 from .views import (
     HomePageView,
-    ArticleListView
+    ArticleListView,
+    ArticleDetailView,
+    ArticleCreateView,
+    ArticleUpdateView
 )
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
     path("articles/", ArticleListView.as_view(), name="article_list"),
+    path("articles/<int:pk>/", ArticleDetailView.as_view(), name="article_detail"),
+    path("articles/new", ArticleCreateView.as_view(), name="article_new"),
+    path("articles/<int:pk>/edit/", ArticleUpdateView.as_view(), name="article_edit"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from django.views.generic import (
     TemplateView, 
     ListView,
+    DetailView,
 )
+from django.views.generic.edit import CreateView, UpdateView
+from django.urls import reverse_lazy
 
 from .models import Article
 
@@ -14,3 +17,18 @@ class ArticleListView(ListView):
     model = Article
     template_name = "article_list.html"
     context_object_name = "articles"
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = "article_detail.html"
+
+class ArticleCreateView(CreateView):
+    model = Article
+    template_name = "article_new.html"
+    fields = ["title", "author", "content"]
+    # success_url = reverse_lazy("article_detail")
+
+class ArticleUpdateView(UpdateView):
+    model = Article
+    template_name = "article_edit.html"
+    fields = ["title", "content"]
