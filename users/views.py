eefb5ea932b68919ff4d787526ea8ee34dc1bba4
Mokowz <ronniemokaya30@gmail.com>
2023-08-28 14:08:51 +0300

    Configure profile to existing users

--- users/views.py
+++ users/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 
 from .forms import CustomUserCreationForm
+from .models import Profile
 
 # Create your views here.
 class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy("login")
     template_name = "signup.html"
+
+class ProfileView(LoginRequiredMixin, TemplateView):
+    model = Profile
+    template_name = "profile.html"
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)        
+
