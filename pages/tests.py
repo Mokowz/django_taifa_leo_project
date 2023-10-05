02c82abd2ed1b5e74e2de2e7473722ae0630ce23
Mokowz <ronniemokaya30@gmail.com>
2023-10-05 16:27:20 +0300

    Add test for creation of model

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.utils import timezone
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+from .models import Article
+from .views import *
+
+# Model Tests
+class ArticleModelTest(TestCase):
+
+    def test_create_article(self):
+        author = get_user_model().objects.create_user(username="testuser", password="password")
+        article = Article.objects.create(
+            title="Test Model",
+            date=timezone.now(),
+            content="Example content",
+            author=author,
+        )
+
+        self.assertEqual(article.title, "Test Model")
+        self.assertEqual(article.author, author)
+
