b21848d461309dc141f191176ce905222c40e980
Mokowz <ronniemokaya30@gmail.com>
2023-10-05 16:30:45 +0300

    Add get absolute url test

--- pages/tests.py
+++ pages/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.utils import timezone
 from django.contrib.auth import get_user_model
+from django.urls import reverse
 
 from .models import Article
 from .views import *
@@ -19,4 +20,18 @@ class ArticleModelTest(TestCase):
 
         self.assertEqual(article.title, "Test Model")
         self.assertEqual(article.author, author)
+
+    def test_get_absolute_url(self):
+        author = get_user_model().objects.create_user(username="testuser", password="password")
+        article = Article.objects.create(
+            title="Test Model",
+            date=timezone.now(),
+            content="Example content",
+            author=author,
+        )
+
+        expected_url = reverse("article_detail", args=[str(article.id)])
+        actual_url = article.get_absolute_url()
+        
+        self.assertEqual(expected_url, actual_url)
 
